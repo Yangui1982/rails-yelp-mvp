ee30654351d959228a552b7ad57011ff1e016119
JeanGuillaume Vicente <jg.vicente@wanadoo.fr>
2022-06-01 09:09:39 +0200

    some corrections

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -16,7 +16,7 @@ class RestaurantsController < ApplicationController
     @restaurant = Restaurant.new(restaurant_params)
 
     if @restaurant.save
-      redirect_to @restaurant
+      redirect_to restaurant_path(@restaurant)
     else
       render :new
     end
@@ -25,6 +25,6 @@ class RestaurantsController < ApplicationController
   private
 
   def restaurant_params
-    params.require(:restaurant).permit(:name, :address, :category)
+    params.require(:restaurant).permit(:name, :address, :phone_number, :category)
   end
 end
